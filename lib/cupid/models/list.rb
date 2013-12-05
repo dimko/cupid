4b2803ecd0044ebfd70ef075768be8eba6a52a9e
Dimko <deemox@gmail.com>
2013-12-05 18:47:04 +0400

    implemented list#add_subscribers

--- lib/cupid/models/list.rb
+++ lib/cupid/models/list.rb
@@ -8,10 +8,14 @@ class Cupid
                  :folder_id    => "Category"
 
       convert(:id) { |id| id.to_i }
-      convert(:folder_id){ |folder_id| folder_id.to_i }
+      convert(:folder_id) { |folder_id| folder_id.to_i }
 
-      belongs_to(:DataFolder){ |list, folder| folder.id == list.folder_id }
+      belongs_to(:DataFolder) { |list, folder| folder.id == list.folder_id }
 
+      def add_subscribers(subscribers)
+        set = SubscriberSet.new(@cupid, :Subscriber, subscribers)
+        set.add_to_list(self).first
+      end
     end
   end
 end
